49064e58e23dda1c07bf02406c2c86d2cac679b6
kindron-vent <kindronventures@gmail.com>
2025-10-26 09:16:49 +0300

    my tests

---
+++ first_letter.py
@@ -0,0 +1,24 @@
+# 4.5 - Challenge: Pick Apart Your User's Input
+# Solution to code challenge
+
+
+# Return the upper-case first letter entered by the user
+
+# user_input = input("Tell me your password: ")
+# first_letter = user_input[0]
+# print("The first letter you entered was: " + first_letter.upper())
+
+#using strings with arithmetic operators
+# num = "2"
+# num + num
+# print(num + num)
+# print(num * 3)
+# print(3*num)
+num = input("Enter a number to be doubled: ")
+doubled_num = float(num) * 2
+print(doubled_num)
+#convert a string to a number
+# use: int() for integer and float() for floating point number
+
+int("12")
+print(float("12"))
